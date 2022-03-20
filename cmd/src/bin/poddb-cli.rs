3f67629999ecf536686aba910e6ba3aa1d811e11
kklinan <kklinang@gmail.com>
2022-03-20 21:38:54 +0800

    feat(cli): add demo

--- cmd/src/bin/poddb-cli.rs
+++ cmd/src/bin/poddb-cli.rs
@@ -14,6 +14,53 @@
  limitations under the License.
  */
 
+use clap::{Parser, Subcommand};
+ 
+/// PodDB
+#[derive(Parser)]
+#[clap(name = "poddb-cli")]
+#[clap(version = "0.1.0", about = None)]
+#[warn(unreachable_patterns)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+
+    /// Server hostname (default: 127.0.0.1).
+    #[clap(short, default_value = "127.0.0.1")]
+    hostname: String,
+
+    /// Server port (default: 6379).
+    #[clap(short, default_value_t = 6379)]
+    port: u16,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+     /// Set a key.
+     #[clap(arg_required_else_help = true)]
+     Set {
+         /// The name of key.
+         key: String,
+         value: String,
+     },
+     
+    /// Get a key.
+    #[clap(arg_required_else_help = true)]
+    Get {
+        /// The name of key.
+        key: String,
+    },
+}
+
 fn main() {
-    println!("Hello, poddb-cli!");
+    let args = Cli::parse();
+
+    match &args.command {
+        Commands::Set { key, value } => {
+            println!("set {} = {}", key, value);
+        }
+        Commands::Get { key } => {
+            println!("get {}", key);
+        }
+    }
 }
